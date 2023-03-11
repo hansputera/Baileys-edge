9ab4fb2ce952b93d9ab05bd86b0d4ff793062611
Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>
2023-03-11 10:38:32 +0000

    feat: init
    Signed-off-by: Hanif Dwy Putra S <hanifdwyputrasembiring@gmail.com>

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  'env': {
+    'es2021': true,
+  },
+  'extends': 'google',
+  'overrides': [
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module',
+  },
+  'rules': {
+  },
+};
